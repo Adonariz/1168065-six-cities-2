87de31fbb8b846d180df8932205cbe82371f9363
Adonariz <raikkonen07@live.ru>
2026-02-15 21:16:09 +0300

    adds default command and process comand method

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,3 +1,4 @@
+import { CommandParser } from './command-parser.js';
 import { Command } from './commands/command.interface.js';
 
 type CommandCollection = Record<string, Command>;
@@ -5,6 +6,8 @@ type CommandCollection = Record<string, Command>;
 /** CLI commands manager */
 export class CliApplication {
   private commands: CommandCollection = {};
+
+  constructor(private readonly defaultCommand: string = '--help') {}
 
   public registerCommands(commandList: Command[]): void {
     commandList.forEach((command) => {
@@ -16,5 +19,27 @@ export class CliApplication {
 
       this.commands[commandName] = command;
     });
+  }
+
+  public getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  public getDefaultCommand(): Command | never {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(
+        `The default command (${this.defaultCommand}) is not registered.`,
+      );
+    }
+
+    return this.commands[this.defaultCommand];
+  }
+
+  public processCommand(argv: string[]): void {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArgs = parsedCommand[commandName] ?? [];
+    command.execute(...commandArgs);
   }
 }
