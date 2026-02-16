c1bfc4c111bfe6102162a04d0af12186e34b73fc
Adonariz <raikkonen07@live.ru>
2026-02-16 21:07:55 +0300

    adds types for offer and user

---
+++ src/shared/facility.enum.ts
@@ -0,0 +1,9 @@
+export enum Facility {
+  Breakfast = 'Breakfast',
+  AC = 'Air conditioning',
+  Workspace = 'Laptop friendly workspace',
+  BabySeat = 'Baby seat',
+  Washer = 'Washer',
+  Towels = 'Towels',
+  Fridge = 'Fridge',
+}
---
+++ src/shared/offer.type.ts
@@ -0,0 +1,25 @@
+import { City } from './city.enum.js';
+import { Coordinates } from './coordinates.type.js';
+import { Facility } from './facility.enum.js';
+import { HousingType } from './housing-type.enum.js';
+import { User } from './user.type.js';
+
+export type Offer = {
+  title: string; // 10–100
+  description: string; // 20–1024
+  publishDate: Date;
+  city: City;
+  previewImage: string;
+  images: string[]; // всегда 6
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number; // 0–5 (1 знак после запятой)
+  type: HousingType;
+  rooms: number; // 1–8
+  guests: number; // 1–10
+  price: number; // 100–100000
+  amenities: Facility[];
+  author: User;
+  commentsCount: number; // рассчитывается автоматически
+  location: Coordinates;
+};
