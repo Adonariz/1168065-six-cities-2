17dde6319adf97273daf4789d975139553fdf3a2
Adonariz <raikkonen07@live.ru>
2026-02-28 23:20:56 +0300

    adds file writer

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,9 +1,10 @@
-import { appendFile } from 'node:fs/promises';
 import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
 import { MockServerData } from '../../shared/types/mock-server-data.type.js';
 import { Command } from './command.interface.js';
 import got from 'got';
 import chalk from 'chalk';
+import { getErrorMessage } from '../../shared/helpers/index.js';
+import { TSVFileWriter } from '../../shared/libs/file-writer/index.js';
 
 export class GenerateCommand implements Command {
   private initialData: MockServerData;
@@ -18,11 +19,10 @@ export class GenerateCommand implements Command {
 
   private async write(filepath: string, offerCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
 
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(filepath, `${tsvOfferGenerator.generate()}\n`, {
-        encoding: 'utf8',
-      });
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
@@ -39,11 +39,7 @@ export class GenerateCommand implements Command {
       await this.write(filepath, offerCount);
       console.info(chalk.greenBright(`File ${filepath} was created`));
     } catch (error: unknown) {
-      console.error(chalk.redBright('Failed to generate data'));
-
-      if (error instanceof Error) {
-        console.error(chalk.redBright(error.message));
-      }
+      console.error(chalk.redBright(getErrorMessage(error)));
     }
   }
 }
