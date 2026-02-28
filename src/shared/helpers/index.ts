c10da7a62d0c350a5d6474017800858a1d3b75cf
Adonariz <raikkonen07@live.ru>
2026-02-28 22:53:39 +0300

    adds streams to file reader and import command

--- src/shared/helpers/index.ts
+++ src/shared/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './types-helpers.js';
 export * from './mock-helpers.js';
 export * from './offer.js';
+export * from './common.js';
--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,70 +1,36 @@
-import { readFileSync } from 'node:fs';
+import { createReadStream } from 'node:fs';
 import { FileReader } from './file-reader.interface.js';
-import { City, Offer, parseFacility, parseHousing } from '../../types/index.js';
-import { booleanFromString } from '../../helpers/index.js';
+import EventEmitter from 'node:events';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(private readonly filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim())
-      .map((line) => line.split('\t'))
-      .map(
-        ([
-          title,
-          description,
-          publishDate,
-          city,
-          previewImage,
-          images,
-          isPremium,
-          isFavorite,
-          rating,
-          type,
-          rooms,
-          guests,
-          price,
-          facilities,
-          name,
-          email,
-          location,
-        ]) => ({
-          title,
-          description,
-          publishDate: new Date(publishDate),
-          city: City[city as keyof typeof City],
-          previewImage,
-          images: images.split(';'),
-          isPremium: booleanFromString(isPremium),
-          isFavorite: booleanFromString(isFavorite),
-          rating: parseFloat(rating),
-          type: parseHousing(type),
-          rooms: parseInt(rooms, 10),
-          guests: parseInt(guests, 10),
-          price: parseInt(price, 10),
-          facilities: facilities
-            .split(';')
-            .map((facility) => parseFacility(facility)),
-          host: {
-            name,
-            email,
-          },
-          location: (() => {
-            const [latitude, longitude] = location.split(';');
-            return {
-              latitude: parseFloat(latitude),
-              longitude: parseFloat(longitude),
-            };
-          })(),
-        }),
-      );
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
+    }
+
+    this.emit('end', importedRowCount);
   }
 }
