0356e5517449a04867e3ef49407e86bc069ab0c8
Adonariz <raikkonen07@live.ru>
2026-03-11 22:51:02 +0300

    adds rest config

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,10 +1,12 @@
-import { RestApplication } from './rest/rest.application.js';
-import { PinoLogger } from './shared/libs/logger/pino.logger.js';
+import { RestApplication } from './rest/index.js';
+import { PinoLogger } from './shared/libs/logger/index.js';
+import { RestConfig } from './shared/libs/config/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
+  const config = new RestConfig(logger);
 
-  const app = new RestApplication(logger);
+  const app = new RestApplication(logger, config);
   await app.init();
 }
 
