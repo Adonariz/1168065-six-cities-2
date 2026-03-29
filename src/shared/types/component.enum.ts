883aa38d8e2cabde5a85a9b5e5210219568d83eb
Adonariz <raikkonen07@live.ru>
2026-03-29 15:20:21 +0300

    adds user service and splits containers

--- src/shared/types/component.enum.ts
+++ src/shared/types/component.enum.ts
@@ -3,4 +3,6 @@ export const Component = {
   Logger: Symbol.for('Logger'),
   Config: Symbol.for('Config'),
   DatabaseClient: Symbol.for('DatabaseClient'),
+  UserServise: Symbol.for('UserService'),
+  UserModel: Symbol.for('UserModel'),
 } as const;
